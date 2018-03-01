4a185cbff2eccc2ff2d437f6829044f924436ed3
Jerry Lee <jwl62@georgetown.edu>
2018-03-01 10:05:46 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,7 @@ def reformat_languages(languages)
   languages.each do |type, level_two|
     level_two.each do |language, level_three|
       level_three.each do |type_key, type_val|
-        new_hash[language]={:type => type_val}
+        new_hash[language]={:type => type_val, :style => []}
         new_hash[language][:style] << type
       end
     end
