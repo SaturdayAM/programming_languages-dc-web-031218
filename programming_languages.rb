9e347b7c5800a9e267bfbfa598ae23dfbee27439
Jerry Lee <jwl62@georgetown.edu>
2018-03-01 09:59:17 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,6 +3,7 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |type, level_two|
     level_two.each do |language, level_three|
+      puts language
       new_hash[language]= {}
     end
   end
